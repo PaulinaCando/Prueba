4fc2a3fb0645d831feb3ac36b097dfa26feb87de
PaulinaCando <paulina.garcia.cando@estudiante.uacm.edu.mx>
2021-10-22 09:01:50 -0500

    Viernes

--- Django/hola/hola/urls.py
+++ Django/hola/hola/urls.py
@@ -19,4 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('principal/', include('principal.urls')),
+    path('viernes/', include('viernes.urls')),
+    
 ]
